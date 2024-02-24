e93212933931ac1d7954d9a8ea2781e215bbf0f5
aniszariat <zariatanis.az@gmail.com>
2024-02-24 13:13:35 +0100

    API controller & methods settings

---
+++ app/Http/Controllers/Api/PassportController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PassportController extends Controller
+{
+    //post
+    public function Register(Request $request)
+    {
+    }
+    //post
+    public function Login(Request $request)
+    {
+    }
+    //get
+    public function Profile()
+    {
+    }
+    //get
+    public function Logout()
+    {
+    }
+}
